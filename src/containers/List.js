103e3a0c8fe0b223509d91ec5a0bc2905b0dce9e
unknown <iden.ticlla@gmail.com>
2021-04-02 18:57:38 -0400

    first commit

---
+++ src/containers/List.js
@@ -0,0 +1,21 @@
+import React, { Component  } from "react";
+
+import Card from '../components/Card/Card'
+
+
+
+class List extends Component {
+
+    async componentDidMount() {
+        const res = await fetch('../../assets/data.json')
+        const resJSON = await res.json()
+        console.log(resJSON)
+    }
+    render() {
+        return (
+            <h1>List</h1>
+        )
+    }
+}
+
+export default List
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import List from './containers/List'
+
+ReactDOM.render(<List></List>, document.getElementById('root'))
